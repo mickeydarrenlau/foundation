59839cbbacf70973742879c026c96879fc607294
Liv Gorence <liv@gorence.io>
2023-01-26 21:03:00 -0800

    Initial pass on release workflow.

---
+++ tools/organize-artifacts.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -e
+
+rm -rf artifacts/
+
+mkdir -p artifacts/
+mkdir -p artifacts/build/manifests
+cp build/manifests/update.json artifacts/build/manifests/
+
+find . -name "*-plugin.jar" | while read -r jar
+do
+  DN=`dirname ${jar}`
+  mkdir -p "artifacts/$DN"
+  cp $jar "artifacts/$jar"
+done
