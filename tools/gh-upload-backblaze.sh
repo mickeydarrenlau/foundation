811dc1f2bfc7b0481cc36dd7c03bef213435b50d
Alex Zenla <alex@fork.run>
2023-01-28 21:26:20 -0800

    Fix Backblaze B2 Uploader

--- tools/gh-upload-backblaze.sh
+++ tools/gh-upload-backblaze.sh
@@ -2,8 +2,13 @@
 set -e
 
 BACKBLAZE_B2_VERSION="3.6.0"
+BACKBLAZE_B2_PLATFORM="linux"
+BACKBLAZE_B2_REPO="https://github.com/Backblaze/B2_Command_Line_Tool"
 
-curl -sL -o b2 "https://github.com/Backblaze/B2_Command_Line_Tool/releases/download/v${BACKBLAZE_B2_VERSION}/b2-linux"
+[ "$(uname -s)" = "Darwin" ] && BACKBLAZE_B2_PLATFORM="darwin"
+
+curl --fail -sL -o b2 "${BACKBLAZE_B2_REPO}/releases/download/v${BACKBLAZE_B2_VERSION}/b2-${BACKBLAZE_B2_PLATFORM}"
 chmod +x b2
 ./b2 authorize-account "${ARTIFACTS_KEY_ID}" "${ARTIFACTS_APP_KEY}"
-./b2 sync --delete --replaceNewer artifacts/ "b2:///${ARTIFACTS_BUCKET}/foundation/"
+./b2 sync --delete --replaceNewer --allowEmptySource artifacts/ "b2://${ARTIFACTS_BUCKET}/foundation/"
+rm b2
